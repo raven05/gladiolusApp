7a71ffd6433fd067b742c6f89b14af8418cea716
Adithi Girish <adithig20@gmail.com>
2022-07-19 16:49:10 -0700

    changed colors

--- gladiolusApp/ViewController.swift
+++ gladiolusApp/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quizButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(red:0.86, green: 0.75, blue: 0.63, alpha:1.00)
+        
+
     }
 
+    
 
 }
 
