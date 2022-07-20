dabe9e3c74e6004dc3c506b5bf8655de2f9003bd
Raven Mae Estacio <raven.estacio17@gmail.com>
2022-07-20 14:06:23 -0700

    added resources home and interviews VC

---
+++ gladiolusApp/InterviewsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InterviewsViewController.swift
+//  gladiolusApp
+//
+//  Created by Scholar on 7/20/22.
+//
+
+import UIKit
+
+class InterviewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
