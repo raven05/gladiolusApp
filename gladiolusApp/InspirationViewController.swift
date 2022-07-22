c2684799e2d5b1bdecc79178f40b7df9cb6882a2
Raven Mae Estacio <raven.estacio17@gmail.com>
2022-07-22 09:10:48 -0700

    Did resources stuff

--- gladiolusApp/InspirationViewController.swift
+++ gladiolusApp/InspirationViewController.swift
@@ -15,7 +15,33 @@ class InspirationViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func youtubeLink1(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.youtube.com/watch?v=FEeTLopLkEo")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func youtubeLink2(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.youtube.com/watch?v=QijH4UAqGD8")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func youtubeLink3(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.youtube.com/watch?v=vaRzxPxhYkE")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func youtubeLink4(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.youtube.com/watch?v=x7ttdYqguAY")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func youtubeLink5(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://youtu.be/w94uiz3hMk0")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
